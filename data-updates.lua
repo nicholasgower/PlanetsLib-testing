fb5da514a8aaeeb7ffdc111a256d0ea98c008cd6
Nicholas Gower <nicholasgower01@gmail.com>
2026-06-10 17:39:27 -0700

    Update data-updates.lua

--- data-updates.lua
+++ data-updates.lua
@@ -1,5 +1,8 @@
 
 
 for _,planet in pairs(data.raw["planet"]) do
-    PlanetsLib.create_planet_entity_variant(planet.name,data.raw["assembling-machine"]["chemical-plant"],{crafting_speed = math.random(1,50)})
+    if planet.name ~= "nauvis" then
+        PlanetsLib.create_planet_entity_variant(planet.name,data.raw["assembling-machine"]["chemical-plant"],{crafting_speed = math.random(1,50)})
+    end
+    
 end
