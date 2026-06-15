b94eb9c15d74e2f07126fc0c0870866d18ddbb36
Nicholas Gower <nicholasgower01@gmail.com>
2026-06-15 00:12:22 -0700

    Added rocket_part_multiplier tester

--- data-updates.lua
+++ data-updates.lua
@@ -3,6 +3,15 @@
 for _,planet in pairs(data.raw["planet"]) do
     if planet.name ~= "nauvis" then
         PlanetsLib.create_planet_entity_variant(planet.name,data.raw["assembling-machine"]["chemical-plant"],{crafting_speed = math.random(1,50)})
+        PlanetsLib:update{
+            {
+                type = "planet",
+                name = planet.name,
+                special_properties = {
+                    rocket_part_multiplier = planet.surface_properties["gravity"]/10
+                }
+            }
+        }
     end
     
 end
