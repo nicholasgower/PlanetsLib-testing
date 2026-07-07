413b492c383857959d780c5903ec39367eaba5c9
Nicholas Gower <nicholasgower01@gmail.com>
2026-07-06 17:38:49 -0700

    Update data-updates.lua

--- data-updates.lua
+++ data-updates.lua
@@ -10,3 +10,18 @@ for _,planet in pairs(data.raw["planet"]) do
     end
     
 end
+
+PlanetsLib.create_planet_entity_variant("space-platform",data.raw["assembling-machine"]["chemical-plant"],{crafting_speed = math.random(1,50)},"enable-chemical-plant-replacement")
+
+for entity_type,entity_table in pairs(data.raw) do
+    if PlanetsLib.rro.contains({"assembling-machine","furnace"},entity_type) then
+        for entity_name,entity_table in pairs(entity_table) do
+            if not string.find(entity_name,"PlanetsLib") then
+                 PlanetsLib.create_planet_entity_variant("nauvis",data.raw[entity_type][entity_name],{crafting_speed = data.raw[entity_type][entity_name].crafting_speed*1.5},"enable-chemical-plant-replacement")
+            end
+           
+        end
+        
+    end
+end
+
